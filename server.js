0cc68c51987fc58a9ec5f7889440c46cd28a9e4a
Eagl9Eye <janhrtg@googlemail.com>
2020-10-14 16:51:29 +0200

    Added HTMLparsing to get Names of markers

--- server.js
+++ server.js
@@ -1,51 +1,60 @@
 const express = require('express');
+const HTMLparser = require('node-html-parser');
 const { argv } = require('yargs')
-.option('port', {
-  alias: 'p',
-  describe: 'provide the port where the server will be aviable'
-})
-.option('host', {
-  alias: 'h',
-  describe: 'provide the host address of the server'
-})
-.option('origin', {
-  alias: 'o',
-  type: 'string',
-  describe: 'specify the origin of the manipulated json-file',
-  demandOption: true
-})
-.option('naming', {
-  alias: 'n',
-  describe: 'specify naming rules for the manipulated json-file'
-})
-.default('port', 8000, '(8000)')
-.default('host', "0.0.0.0", '(this IP-address)')
-.help();
+    .option('port', {
+        alias: 'p',
+        describe: 'provide the port where the server will be aviable'
+    })
+    .option('host', {
+        alias: 'h',
+        describe: 'provide the host address of the server'
+    })
+    .option('origin', {
+        alias: 'o',
+        type: 'string',
+        describe: 'specify the origin of the manipulated json-file',
+        demandOption: true
+    })
+    .default('port', 8000, '(8000)')
+    .default('host', "0.0.0.0", '(this IP-address)')
+    .help();
 const fetch = require('node-fetch');
 var HOST = argv.host,
-PORT = argv.port,
-URL = argv.origin, 
-names = argv.naming?require(argv.naming) : {},
-app = express();
+    PORT = argv.port,
+    URL = argv.origin,
+    //names = argv.naming ? require(argv.naming) : {},
+    app = express();
 
-async function getOriginal(url) {
-  return await(fetch(url).then(res => res.json()));
+async function getOriginalasJSON(url) {
+    return await (fetch(url).then(res => res.json()));
 }
 
-function processParams(params){
-  let res = {};
-  params["marker state"].split("").forEach((c,i) => res[names[i+1]?names[i+1]:i+1] = c)
-  return res;
+async function getOriginalText(url) {
+    return await (fetch(url).then(res => res.text()));
 }
 
-app.get('/params', async function (req, res) {
-  var params = await getOriginal(URL)
-  res.send(processParams(params));
-  console.log("visited on: " + Date().toString());
+function processParams(params, names) {
+    let res = {};
+    params["marker state"].split("").forEach((c, i) => res[names[i] ? names[i] : i + 1] = c)
+    return res;
+}
+
+function processNames(html) {
+    const root = HTMLparser.parse(html);
+    var names = {};
+    root.querySelectorAll('.mk').forEach(n => names[n.id.slice(1)] = n.innerText)
+    return names;
+}
+
+app.get('/params', async function(req, res) {
+    var params = await getOriginalasJSON(URL + "/params.json")
+    var html = await getOriginalText(URL)
+    res.send(processParams(params, processNames(html)));
+    console.log("visited on: " + Date().toString());
 });
 
 console.log(`Origin is set as: ${URL}`);
 
-app.listen(PORT, function () {
-  console.log(`Listening on ${HOST}:${PORT}`);
+app.listen(PORT, function() {
+    console.log(`Listening on ${HOST}:${PORT}`);
 });
