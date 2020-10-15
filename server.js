1c7892b97b8cd1ec2ed7b56dfd45e38eca17b53c
Eagl9Eye <janhrtg@googlemail.com>
2020-10-15 12:59:18 +0200

    Made origin changeable via http-post request through "/server"

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const fs = require('fs');
 const HTMLparser = require('node-html-parser');
+const bodyParser = require("body-parser");
 const { argv } = require('yargs')
     .option('port', {
         alias: 'p',
@@ -9,27 +11,22 @@ const { argv } = require('yargs')
         alias: 'h',
         describe: 'provide the host address of the server'
     })
-    .option('origin', {
-        alias: 'o',
-        type: 'string',
-        describe: 'specify the origin of the manipulated json-file',
-        demandOption: true
-    })
     .default('port', 8000, '(8000)')
     .default('host', "0.0.0.0", '(this IP-address)')
     .help();
 const fetch = require('node-fetch');
 var HOST = argv.host,
     PORT = argv.port,
-    URL = argv.origin,
-    //names = argv.naming ? require(argv.naming) : {},
-    app = express();
+    config = "./config.json"
+app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 async function getOriginalasJSON(url) {
     return await (fetch(url).then(res => res.json()));
 }
 
-async function getOriginalText(url) {
+async function getOriginalasText(url) {
     return await (fetch(url).then(res => res.text()));
 }
 
@@ -46,16 +43,55 @@ function processNames(html) {
     return names;
 }
 
-app.get('/params', async function(req, res) {
-    var params = await getOriginalasJSON(URL + "/params.json")
-    var html = await getOriginalText(URL)
-    res.send(processParams(params, processNames(html)));
-    console.log("visited on: " + Date().toString());
+function getOrigin() {
+    try {
+        obj = JSON.parse(fs.readFileSync(config, 'utf8'));
+        return obj.origin;
+    } catch (e) { console.error(e); throw 404; }
+}
+
+async function setOrigin(newOrigin) {
+    if (!newOrigin) { return 404; }
+    const jsonString = JSON.stringify({ origin: newOrigin })
+
+    return new Promise(function (resolve, reject) {
+        fs.writeFile(config, jsonString, err => {
+            if (err) {
+                reject(404);
+            }
+            else {
+                resolve(200)
+            }
+        });
+    });
+}
+
+app.post('/server', async function (req, res) {
+    let status = await setOrigin(req.body.origin);
+    if(status == 200){
+        console.log(`New origin is set to: ${req.body.origin}`)}
+    res.sendStatus(status);
+})
+
+app.get('/params', async function (req, res) {
+    let URL;
+    process.stdout.write("visited on: " + Date().toString() + "[");
+    try {
+        URL = getOrigin();
+        var params = await getOriginalasJSON(URL + "/params.json")
+        var html = await getOriginalasText(URL)
+        res.send(processParams(params, processNames(html)));
+        console.log(URL + "]")
+    } catch (e) {
+        res.sendStatus(404)
+        console.log("failed]")
+    }
 });
 
 console.log("Starting service!" + Date().toString());
-console.log(`Origin is set as: ${URL}`);
+console.log("Origin is set as:");
+try { console.log("\t" + getOrigin()); } catch (e) { }
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log(`Listening on ${HOST}:${PORT}`);
 });
